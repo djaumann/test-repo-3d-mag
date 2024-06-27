ab5005cee6a9c9ae2aabdd0045fcc90b9610fbe5
Jaumann  Dominik  (DES DOS SW ESW EPE) <Dominik.Jaumann@infineon.com>
2024-06-27 13:19:12 +0200

    Edited conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinxemoji.sphinxemoji',
     'sphinx_tabs.tabs',
